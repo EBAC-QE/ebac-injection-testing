ff343e7445997b5eae34568d6183dd33b2e3f0c7
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2022-03-15 23:04:17 -0300

    sql injection tests

--- test/sql.spec.js
+++ test/sql.spec.js
@@ -3,5 +3,15 @@ const API_URL = process.env.API_URL
 
 describe('User Login', () => {
     it('SQL Injection', async () => {
+        await req(API_URL)
+            .post('/sql/login')
+            .send({
+                "email": "test@meuemail.com.br",
+                "password": "' or 1=1 --"
+            })
+            .set("Accept", "application/json")
+            .then(response =>{
+                expect(response.statusCode).toEqual(422)
+            })
     });
 });
